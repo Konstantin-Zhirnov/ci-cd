cd2a0c144ce75c65493ea298699d3c205d911e34
Konstantin Zhirnov <zhirnov-11.06@yandex.ru>
2022-06-26 21:52:30 +0300

    fix delete Paper

--- src/pages/UsersPage/UsersTable/index.tsx
+++ src/pages/UsersPage/UsersTable/index.tsx
@@ -6,7 +6,6 @@ import TableCell from '@mui/material/TableCell'
 import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
-import Paper from '@mui/material/Paper'
 
 import { useAppSelector } from '../../../redux/hooks'
 import { getUsers } from '../../../redux/users'
