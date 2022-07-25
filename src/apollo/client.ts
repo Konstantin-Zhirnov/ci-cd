cb3999da8d02e054e94177ef22ff53d6cff786d7
Konstantin Zhirnov <zhirnov-11.06@yandex.ru>
2022-07-25 20:31:25 +0300

    URL fixed

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://mellifluous-lily-395be2.netlify.app:4000',
   cache: new InMemoryCache(),
 })
 
