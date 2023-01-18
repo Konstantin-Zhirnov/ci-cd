075b31b42de0948d027b7c5d45f665ff223c1141
Konstantin Zhirnov <zhirnov-11.06@yandex.ru>
2023-01-17 21:27:31 -0800

    apollo

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,9 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 
-const client = new ApolloClient({
-  uri: 'https://relaxed-cascaron-bc9642.netlify.app/',
-  // uri: 'https://safe-spire-75667.herokuapp.com/graphql',
+export const client = new ApolloClient({
+  uri: 'https://heartfelt-salmiakki-def3aa.netlify.app/',
   cache: new InMemoryCache(),
 })
-
-export default client
